8b7e05a093b971d8f85fcabac62ce7fc935b861d
Seb3010 <sebastianpdlp@outlook.com>
2026-01-21 02:30:08 +0000

    agregar rutas home, contacto y envío de formulario

--- Flask/app.py
+++ Flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # Creamos la aplicación Flask
 app = Flask(__name__)
@@ -13,6 +13,20 @@ def home():
 
     return render_template('index.html', user=nombre, lang=lenguaje, hob=hobies)
 
+@app.route('/contacto')
+def contacto():
+    return render_template('contacto.html')
+
+@app.route('/enviar', methods=['POST'])
+def enviar():
+    # 1. Atrapamos los datos
+    nombre = request.form.get('usuario')
+    msg = request.form.get('mensaje')
+    
+    # 2. Podemos pasar esos datos a una nueva plantilla de "éxito"
+    return render_template('exito.html', persona=nombre, texto=msg)
+
 # Arrancamos el servidor
 if __name__ == '__main__':
     app.run(debug=True)
+
